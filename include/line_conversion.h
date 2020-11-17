70b97b0956e94da869d7383b03f2a3dc94486d46
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-11-17 16:53:12 +0300

    remove old conversion header

--- include/line_conversion.h
+++
@@ -1,9 +0,0 @@
-#include "stddef.h"
-
-
-char* ConvertLine(const char* line);
-
-char* CreateConvertedLine(const char** words,
-                          size_t words_size,
-                          int digits_sum);
-
