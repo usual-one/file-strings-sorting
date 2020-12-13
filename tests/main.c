0ab776962a4c5dcdc8f2db67522f714d9a60b6f0
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-12-14 00:07:18 +0300

    add tests executable

--- tests/main.c
+++ tests/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+#include "include/test_utils.h"
+#include "include/test_global.h"
+
+int TESTS_RUN = 0;
+int TESTS_SUCCEEDED = 0;
+int TESTS_FAILED = 0;
 
 int main(int argc, char* argv[]) {
+    TestUtils();
+
+    printf("%d tests run: %d succeeded, %d failed\n", TESTS_RUN, TESTS_SUCCEEDED, TESTS_FAILED);
     return 0;
 }
