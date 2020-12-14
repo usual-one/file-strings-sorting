5999d78184bf72b853d553ef161ed8572d625313
Alexander Kostarev <kostarev.a.p@gmail.com>
2020-12-14 12:39:56 +0300

    add conversion unit tests

--- tests/main.c
+++ tests/main.c
@@ -2,6 +2,7 @@
 
 #include "include/test_utils.h"
 #include "include/test_types_split.h"
+#include "include/test_conversion.h"
 #include "include/test_global.h"
 
 int TESTS_RUN = 0;
@@ -11,6 +12,7 @@ int TESTS_FAILED = 0;
 int main(int argc, char* argv[]) {
     TestUtils();
     TestTypesSplit();
+    TestConversion();
 
     printf("%d tests run: %d succeeded, %d failed\n", TESTS_RUN, TESTS_SUCCEEDED, TESTS_FAILED);
     return 0;
